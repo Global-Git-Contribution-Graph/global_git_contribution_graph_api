6f31e798115e93f0a147767dbc96af643bc2aaf4
mguilbert <matheo.guilbert49@gmail.com>
2026-02-17 15:42:23 +0100

    implementation of the GitHub API call

--- src/graphql/schema.rs
+++ src/graphql/schema.rs
@@ -4,25 +4,42 @@ use async_graphql::{Object, Context, SimpleObject};
 use crate::state::AppState;
 
 #[derive(SimpleObject)]
+struct DailyContribution {
+    date: String,
+    contribution_count: i64
+}
+
+#[derive(SimpleObject)]
 struct Stats {
-    contributions: i32,
-    username: String,
+    history: Vec<DailyContribution>
 }
 
 pub struct QueryRoot;
 
 #[Object]
 impl QueryRoot {
-    async fn stats(&self, ctx: &Context<'_>, name: String) -> Option<Stats> {
+    async fn stats(&self, ctx: &Context<'_>, name: String, username: String, token: String) -> Option<Stats> {
         let state = ctx.data::<Arc<AppState>>().ok()?;
 
         let provider = state.providers.iter()
             .find(|p| p.get_name().to_lowercase() == name.to_lowercase())?;
 
-        Some(Stats {
-            contributions: 42,
-            username: format!("User sur {}", provider.get_name()),
-        })
+        let raw_stats = match provider.get_stats(&username, &token).await {
+            Ok(data) => data,
+            Err(e) => {
+                println!("Erreur lors de l'appel GitHub : {}", e);
+                return None;
+            }
+        };
+
+        let history = raw_stats.into_iter()
+            .map(|(date, count)| DailyContribution { 
+                date, 
+                contribution_count: count 
+            })
+            .collect();
+
+        Some(Stats { history })
     }
 }
 
--- src/providers.rs
+++ src/providers.rs
@@ -1,11 +1,112 @@
+use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+
+#[async_trait]
 pub trait GitProvider {
     fn get_name(&self) -> String;
+    async fn get_stats(&self, username: &str, token: &str) -> Result<Vec<(String, i64)>, String>;
 }
 
 pub struct GitHub;
 
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct ContributionDay {
+    date: String,
+    contribution_count: i64
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Week {
+    contribution_days: Vec<ContributionDay>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct ContributionCalendar {
+    weeks: Vec<Week>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct ContributionsCollection {
+    contribution_calendar: ContributionCalendar
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct User {
+    contributions_collection: ContributionsCollection
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Data {
+    user: User
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct GithubResponse {
+    data: Data
+}
+
+#[async_trait]
 impl GitProvider for GitHub {
     fn get_name(&self) -> String {
         "GitHub".to_string()
+    }
+
+    async fn get_stats(&self, username: &str, token: &str) -> Result<Vec<(String, i64)>, String> {
+        let query = "
+            query($login: String!) {
+                user(login: $login) {
+                    contributionsCollection {
+                        contributionCalendar {
+                            weeks {
+                                contributionDays {
+                                    date
+                                    contributionCount
+                                }
+                            }
+                        }
+                    }
+                }
+            }";
+
+        let body = json!({
+            "query": query,
+            "variables": {
+                "login": username
+            }
+        });
+
+        let client = reqwest::Client::new();
+        let res = client.post("https://api.github.com/graphql")
+            .header("Authorization", format!("bearer {}", token))
+            .header("User-Agent", "GGCG-App")
+            .json(&body)
+            .send()
+            .await
+            .map_err(|e| e.to_string())?;
+
+        let github_resp: GithubResponse = res.json::<GithubResponse>()
+            .await
+            .map_err(|e| e.to_string())?;
+
+        let mut formatted_contributions: Vec<(String, i64)> = Vec::new();
+        for week in github_resp.data.user.contributions_collection.contribution_calendar.weeks {
+            for day in week.contribution_days {
+                if day.contribution_count == 0 {
+                    continue;
+                }
+
+                formatted_contributions.push((day.date.to_string(), day.contribution_count));
+            }
+        }
+
+        Ok(formatted_contributions)
     }
 }
