b60aa26f029901a749229b06d3740b2557ca4cc1
mguilbert <matheo.guilbert49@gmail.com>
2026-02-18 11:49:31 +0100

    make stats take a Vec of the ForgeInput structure to combine all git providers configured by the user

--- src/graphql/schema.rs
+++ src/graphql/schema.rs
@@ -1,5 +1,5 @@
-use std::sync::Arc;
-use async_graphql::{Object, Context, SimpleObject};
+use std::{collections::HashMap, sync::Arc};
+use async_graphql::{Object, Context, SimpleObject, InputObject};
 
 use crate::state::AppState;
 
@@ -14,30 +14,47 @@ struct Stats {
     history: Vec<DailyContribution>
 }
 
+#[derive(InputObject)]
+struct ForgeInput {
+    name: String,
+    username: String,
+    token: String,
+    url: Option<String>
+}
+
 pub struct QueryRoot;
 
 #[Object]
 impl QueryRoot {
-    async fn stats(&self, ctx: &Context<'_>, name: String, username: String, token: String, url: Option<String>) -> Option<Stats> {
+    async fn stats(&self, ctx: &Context<'_>, forges: Vec<ForgeInput>) -> Option<Stats> {
         let state = ctx.data::<Arc<AppState>>().ok()?;
+        let mut totals: HashMap<String, i64> = HashMap::new();
 
-        let provider = state.providers.iter()
-            .find(|p| p.get_name().to_lowercase() == name.to_lowercase())?;
+        for forge in forges {
+            let provider = state.providers.iter()
+                .find(|p| p.get_name().to_lowercase() == forge.name.to_lowercase())?;
 
-        let raw_stats = match provider.get_stats(&username, &token, url.as_deref()).await {
-            Ok(data) => data,
-            Err(e) => {
-                println!("Erreur lors de l'appel API : {}", e);
-                return None;
+            let raw_stats = match provider.get_stats(&forge.username, &forge.token, forge.url.as_deref()).await {
+                Ok(data) => data,
+                Err(e) => {
+                    println!("Erreur lors de l'appel API : {}", e);
+                    return None;
+                }
+            };
+
+            for (date, count) in raw_stats {
+                *totals.entry(date).or_insert(0) += count;
             }
-        };
+        }
+        
 
-        let history = raw_stats.into_iter()
+        let mut history: Vec<DailyContribution> = totals.into_iter()
             .map(|(date, count)| DailyContribution { 
                 date, 
-                contribution_count: count 
+                contribution_count: count
             })
             .collect();
+        history.sort_by(|a, b| a.date.cmp(&b.date));
 
         Some(Stats { history })
     }
